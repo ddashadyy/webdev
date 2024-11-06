8e00af074a264b3d6bb893e6cb2614c9b7ebb22b
ddashadyy <poletaevgena75@gmail.com>
2024-11-06 19:51:23 +0800

    added auth router

--- second_project/backend/app/route/index.js
+++ second_project/backend/app/route/index.js
@@ -3,10 +3,11 @@ const router = new Router();
 
 const categoryRouter = require("./categoryRouter");
 const productRouter = require("./productRouter");
-
+const authRouter = require("./authRouter");
 
 router.use('/category', categoryRouter);
 router.use('/product', productRouter);
+router.use('/auth', authRouter);
 
 
 module.exports = router;
